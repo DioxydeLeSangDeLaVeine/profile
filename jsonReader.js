f7263d0bc08ecda2afa5145e2f2fea687f1a9284
dioxydecarb <111918997+DioxydeLeSangDeLaVeine@users.noreply.github.com>
2022-10-19 16:07:06 +0200

    Add files via upload

---
+++ jsonReader.js
@@ -0,0 +1,29 @@
+class JavaScriptBasics {
+    getInfo() {
+        return {
+            id: "jsonreaderscripts",
+            name: "JSON Reader",
+            blocks: [
+                {
+                    opcode: "object_getkey",
+                    blockType: Scratch.BlockType.COMMAND,
+                    text: "OBJECT [PARSEDJSON] getKey [KEY]",
+                    arguments: {
+                        PARSEDJSON: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: "{}"
+                        },
+                        KEY: {
+                            type: Scratch.ArgumentType.STRING,
+                            defaultValue: " "
+                        }
+                    }
+                }
+            ]
+        }
+    }
+    object_getkey(args) {
+        return JSON.stringify(JSON.parse(args.PARSEDJSON)[args.KEY])
+    }
+}
+Scratch.extensions.register(new JavaScriptBasics())
