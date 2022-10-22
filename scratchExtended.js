d68b13e6710488d383229233c080d941c454904c
dioxydecarb <111918997+DioxydeLeSangDeLaVeine@users.noreply.github.com>
2022-10-22 23:24:52 +0200

    Update scratchExtended.js

--- scratchExtended.js
+++ scratchExtended.js
@@ -145,7 +145,7 @@ class JavaScriptBasics {
     clientip() {
         fetch('https://api.ipify.org/?format=json')
             .then(results=>results.json())
-            .then(data=>return data.ip)
+            .then(data=>{return data.ip})
     }
 }
 Scratch.extensions.register(new JavaScriptBasics())
